79ca195fd05c4d04a04c0971cd6ab2494abafe5d
Yash Soni <ysoni0303@gmail.com>
2022-10-01 19:26:25 -0700

    Time: 1 ms (89.60%), Space: 43.7 MB (18.58%) - LeetHub

---
+++ 50-powx-n/50-powx-n.java
@@ -0,0 +1,18 @@
+class Solution {
+    double result = 0;
+    public double myPow(double x, int n) {
+        //base
+        if(n == 0) return 1.00;
+        
+        
+        //logic
+        double result = myPow(x,n/2);
+        
+        if(n%2 == 0){
+            return result * result;
+        }else{
+            if(n>0) return result * result * x;
+            else return result * result * (1/x);
+        }
+    }
+}
